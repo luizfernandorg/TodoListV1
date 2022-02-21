a7d3e0d09ede9e03dd9d1677595063fb15bf607a
luizfernandorg <luizfernandorg@gmail.com>
2022-02-21 00:47:00 -0300

    Generate comments

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,9 @@
 //jshint esversion:8
 let counter = 0;
+
+/* make a post-call to the API to insert the new task
+ * and then inject the new line inside the unordered list
+*/
 $("form").on("submit", (e) => {
     e.preventDefault();
     const url = e.target.action;
@@ -33,13 +37,13 @@ $("#newItem").keyup((e) => {
     }
 });
 
-// Create the option to edit the task inserted
+// Create the option to edit the task added
 function editIt(item){
     const textForInput = $(`#${item}`).text().split("delete")[0].trim();
     $(`#${item}`).html(`<input type='text' id="edited" value="${textForInput}"><button id="save" onClick="changeValue('${item}')">Save</button`);
 }
 
-// triggered by the save button, will adapt the li with the new value
+// triggered by the save button. Will adapt the li with the new value
 function changeValue(item){
     const textEdited = $("#edited").val();
     $(`#${item}`).html(`<span>${textEdited}</span> <span><a href='#' onClick="removeLine('${item}')">delete</a> <a href="#" onClick="editIt('${item}')">edit</a></span>`);
